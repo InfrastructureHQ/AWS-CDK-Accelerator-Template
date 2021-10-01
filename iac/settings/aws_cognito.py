cb1c51eb67ecb7c7f47a62283e13ad1670a360a9
AdityaSharma-Versatile <61409450+AdityaSharma-Versatile@users.noreply.github.com>
2021-10-01 13:15:36 +0530

    Settings + Stacks

---
+++ iac/settings/aws_cognito.py
@@ -0,0 +1,24 @@
+from typing import Dict, Optional
+
+import pydantic
+
+from settings.globalsettings import GlobalSettings
+
+globalsettings = GlobalSettings()
+
+AWS_ACCOUNT_ID = globalsettings.AWS_ACCOUNT_ID
+AWS_REGION = globalsettings.AWS_REGION
+
+
+class CognitoSettings(
+    pydantic.BaseSettings
+):  # pylint: disable=too-few-public-methods
+
+    stack_name: str = "APIStack"
+    description: Optional[str] = "API Stack"
+    aws_default_region = "us-east-1"
+    stage: str = "production"
+    email: Optional[str] = "<INSERT_VALUE>"
+    cost_center: Optional[str]
+    api_name: Optional[str] = "<INSERT_VALUE>"
+    api_version: Optional[str] = "<INSERT_VALUE>"
